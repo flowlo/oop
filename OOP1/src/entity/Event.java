82d25fddc6bb0140d7ced863fb0df2489e8f9d99
Dominik Dopplinger <Dominik Dopplinger@8f0689cb-c7f0-493e-b951-6e7372201282>
2012-10-23 21:48:18 +0000

    Event-Verwaltung
    git-svn-id: https://subversion.assembla.com/svn/oop-ws2012@28 8f0689cb-c7f0-493e-b951-6e7372201282

--- OOP1/src/entity/Event.java
+++ OOP1/src/entity/Event.java
@@ -2,7 +2,9 @@ package entity;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Repraesentiert sowohl eine Probe, als auch einen Auftritt
@@ -13,7 +15,7 @@ public class Event extends Item implements Comparable<Event> {
 	/**
 	 * Ort der Veranstaltung (Probe/Auftritt).
 	 */
-	private Location place;
+	private Location location;
 
 	/**
 	 * Dauer dieses Events in Sekunden.
@@ -24,6 +26,12 @@ public class Event extends Item implements Comparable<Event> {
 	 * Typ dieses Events, um zwischen Probe und Auftritt zu unterscheiden.
 	 */
 	private EventType type;
+
+	private List<Event> history = new ArrayList<Event>();
+
+	private boolean blockSave = false;
+
+	private boolean canceled = false;
 
 	/**
 	 * Neues Event erstellen
@@ -39,38 +47,136 @@ public class Event extends Item implements Comparable<Event> {
 	 * @param type
 	 *            typ des Events
 	 */
-	public Event(Location place, Date date, int duration, int money,
-			EventType type) {
-		this.place = place;
-		this.dateTime = date;
+	public Event(Location location, Date dateTime, int duration, int money, EventType type) {
+		this.location = location;
+		this.dateTime = dateTime;
 		this.duration = duration;
 		this.money = money;
 		this.type = type;
 	}
 
-	public Location getPlace() {
-		return place;
+	public Event(Event event) {
+		this.location = event.getLocation();
+		this.dateTime = event.getDateTime();
+		this.duration = event.getDuration();
+		this.comment = event.getComment();
+		this.money = event.getMoney();
+		this.type = event.getType();
+		this.deleted = event.isDeleted();
+		this.canceled = event.isCanceled();
 	}
 
-	public Date getDate() {
-		return dateTime;
+	public EventType getType() {
+		return type;
+	}
+
+	public void setType(EventType type) {
+		save();
+		this.type = type;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		save();
+		this.location = location;
 	}
 
 	public int getDuration() {
 		return duration;
 	}
 
-	public float getMoney() {
-		return money;
+	public void setDuration(int duration) {
+		save();
+		this.duration = duration;
+	}
+
+	public boolean isCanceled() {
+		return canceled;
+	}
+
+	public void setCanceled(boolean canceled) {
+		save();
+		this.canceled = canceled;
+	}
+
+	@Override
+	public void save() {
+		if (!blockSave) {
+			history.add(new Event(this));
+		}
+	}
+
+	@Override
+	public Event getHistoryItem(int version) {
+		if (history.size() > version) {
+			return history.get(version);
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public List<Event> getHistory() {
+		return history;
+	}
+
+	@Override
+	public void revert(int version) {
+		revert(getHistoryItem(version));
+	}
+
+	@Override
+	public void revert(Item version) {
+		if (version != null) {
+			if (version instanceof Event) {
+				Event eventVersion = (Event) version;
+				edit(eventVersion);
+			}
+		}
+	}
+
+	public void edit(Event event) {
+		edit(event.getLocation(), event.getDateTime(), event.getDuration(), event.getMoney(), event.getType(), event.getComment(), event.isDeleted(),
+				event.isCanceled());
+	}
+
+	public void edit(Location location, Date dateTime, Integer duration, Integer money, EventType type, String comment, Boolean deleted,
+			Boolean canceled) {
+		blockSave = true;
+		save();
+		if (location != null) {
+			setLocation(location);
+		}
+		if (dateTime != null) {
+			setDateTime(dateTime);
+		}
+		if (duration != null) {
+			setDuration(duration);
+		}
+		if (money != null) {
+			setMoney(money);
+		}
+		if (comment != null) {
+			setComment(comment);
+		}
+		if (type != null && type != EventType.Dummy) {
+			setType(type);
+		}
+		if (deleted != null) {
+			setDeleted(deleted);
+		}
+		if (canceled != null) {
+			setCanceled(canceled);
+		}
+		blockSave = false;
 	}
 
 	@Override
 	public int compareTo(Event o) {
-		return dateTime.compareTo(o.getDate());
-	}
-
-	public static Event fromDate(Date date) {
-		return new Event(new Location("", ""), date, 1, 0, EventType.Dummy);
+		return dateTime.compareTo(o.getDateTime());
 	}
 
 	@Override
@@ -108,12 +214,15 @@ public class Event extends Item implements Comparable<Event> {
 	 * @return Der Termin
 	 * @throws ParseException
 	 */
-	public static Event toEvent(Location place, String date, String time, int duration, int money, EventType type) throws ParseException {
+	public static Event toEvent(Location location, String dateTime, String time, int duration, int money, EventType type) throws ParseException {
 		SimpleDateFormat df = new SimpleDateFormat("d.M.yyyy-H:m");
 		df.setLenient(false);
-		Date datetime = df.parse(date + "-" + time);
-		return new Event(place, datetime, duration, money, type);
+		Date datetime = df.parse(dateTime + "-" + time);
+		return new Event(location, datetime, duration, money, type);
+	}
 
+	public static Event fromDate(Date date) {
+		return new Event(new Location("", ""), date, 1, 0, EventType.Dummy);
 	}
 
 	public enum EventType {
--- OOP1/src/entity/Item.java
+++ OOP1/src/entity/Item.java
@@ -1,14 +1,16 @@
 package entity;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Oberklasse für alles was mit Geld zu tun hat =)
  * 
  * @author Simon
+ * @author Dominik
  * 
  */
-public class Item {
+public abstract class Item {
 	/**
 	 * Rechnungsbetrag in Cent
 	 */
@@ -24,4 +26,54 @@ public class Item {
 	 */
 	protected Date dateTime;
 
+	boolean deleted;
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		save();
+		this.money = money;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		save();
+		this.comment = comment;
+	}
+
+	public Date getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(Date dateTime) {
+		save();
+		this.dateTime = dateTime;
+	}
+
+	protected abstract void save();
+
+	public abstract List<?> getHistory();
+
+	public abstract Item getHistoryItem(int version);
+
+	public abstract void revert(int version);
+
+	public abstract void revert(Item version);
+
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+
+	public void delete() {
+		setDeleted(false);
+	}
+
+	public boolean isDeleted() {
+		return deleted;
+	}
 }
