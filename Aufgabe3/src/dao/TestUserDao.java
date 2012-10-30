d51634ee568f0c8a4bfa46e1c202c170f95bb47d
dasimsi <dasimsi@8f0689cb-c7f0-493e-b951-6e7372201282>
2012-10-30 19:52:39 +0000

    git-svn-id: https://subversion.assembla.com/svn/oop-ws2012@54 8f0689cb-c7f0-493e-b951-6e7372201282

--- Aufgabe3/src/dao/TestUserDao.java
+++ Aufgabe3/src/dao/TestUserDao.java
@@ -8,20 +8,20 @@ public class TestUserDao implements UserDao {
 
 	private static HashMap<String, User> users = new HashMap<String, User>();
 
-	//Interface erfüllt
+	//Interface erfuellt
 	@Override
 	public User getUser(String loginName) throws DaoException {
 		return users.get(loginName);
 	}
 
-	//FEHLER: User wird einfach überschrieben
+	//FEHLER: User wird einfach ueberschrieben
 	@Override
 	public void createUser(User user) throws DaoException {
 		users.put(user.getLoginName(), user);
 
 	}
 
-	//Interface erfüllt
+	//Interface erfuellt
 	@Override
 	public void deleteUser(String loginName) throws DaoException {
 		users.remove(loginName);
--- Aufgabe3/src/entity/Song.java
+++ Aufgabe3/src/entity/Song.java
@@ -1,33 +1,18 @@
 package entity;
 
-
 /**
- * Ein Song aus dem Repertoire der Band.
- * 
  * @author Lorenz
  */
+//SCHLECHT: Song ist das einzige BandObject (früher hat Member geerbt)
 public class Song extends BandObject {
-	/**
-	 * Laenge des Musikstuecks in Sekunden.
-	 */
+
 	private int duration;
 
-	/**
-	 * Initialisiert ein neues Musikstueck mit Name und Laenge.
-	 * 
-	 * @param name
-	 *            der Titel des Musikstuecks
-	 * @param length
-	 *            die Abspieldauer des Songs
-	 */
 	public Song(String name, int length) {
 		super(name);
 		this.duration = length;
 	}
 
-	/**
-	 * @return Die Abspieldauer des Songs in Sekunden
-	 */
 	public int getDuration() {
 		return duration;
 	}
