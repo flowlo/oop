6b5e4b824353cfc12f9fb15d701068e7c02b984e
dasimsi <dasimsi@8f0689cb-c7f0-493e-b951-6e7372201282>
2012-10-30 19:42:05 +0000

    git-svn-id: https://subversion.assembla.com/svn/oop-ws2012@51 8f0689cb-c7f0-493e-b951-6e7372201282

--- Aufgabe3/src/dao/TestUserDao.java
+++ Aufgabe3/src/dao/TestUserDao.java
@@ -8,17 +8,20 @@ public class TestUserDao implements UserDao {
 
 	private static HashMap<String, User> users = new HashMap<String, User>();
 
+	//Interface erfüllt
 	@Override
 	public User getUser(String loginName) throws DaoException {
 		return users.get(loginName);
 	}
 
+	//FEHLER: User wird einfach überschrieben
 	@Override
 	public void createUser(User user) throws DaoException {
 		users.put(user.getLoginName(), user);
 
 	}
 
+	//Interface erfüllt
 	@Override
 	public void deleteUser(String loginName) throws DaoException {
 		users.remove(loginName);
