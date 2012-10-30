a570b6a028882005a89a2d21a3ffe427877128be
Dominik Dopplinger <Dominik Dopplinger@8f0689cb-c7f0-493e-b951-6e7372201282>
2012-10-30 20:19:07 +0000

    git-svn-id: https://subversion.assembla.com/svn/oop-ws2012@57 8f0689cb-c7f0-493e-b951-6e7372201282

--- Aufgabe3/src/service/ServiceException.java
+++ Aufgabe3/src/service/ServiceException.java
@@ -1,16 +1,9 @@
 package service;
 
 /**
- * Exception class for Service-Layer
- * Services should only throw this Exception (and map all other Exceptions to ServiceException)
- * 
  * @author Simon
  */
 public class ServiceException extends Exception {
-
-	/**
-	 * Automatisch generierte ID.
-	 */
 	private static final long serialVersionUID = -5532751923084103006L;
 
 	public ServiceException()
