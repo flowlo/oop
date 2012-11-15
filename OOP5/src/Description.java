ab08604e930cc352bd8fdee434aefed6e674e277
Dominik Dopplinger <Dominik Dopplinger@8f0689cb-c7f0-493e-b951-6e7372201282>
2012-11-15 16:50:53 +0000

    Description
    git-svn-id: https://subversion.assembla.com/svn/oop-ws2012@79 8f0689cb-c7f0-493e-b951-6e7372201282

---
+++ OOP5/src/Description.java
@@ -0,0 +1,21 @@
+public class Description implements Shorter<Description> {
+
+	public Description(String text) {
+
+	}
+
+	@Override
+	public boolean shorter(Description compare) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "";
+	}
+
+	public int getLines() {
+		return 0;
+	}
+}
