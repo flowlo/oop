7cec84c6945fba2a3e11ab48bdb52d50eca9f55e
lorenz.leutgeb@gmail.com <lorenz.leutgeb@gmail.com@8f0689cb-c7f0-493e-b951-6e7372201282>
2012-11-15 17:31:53 +0000

    Partial implementation of tests 1 and 2, error in OrderedMap
    git-svn-id: https://subversion.assembla.com/svn/oop-ws2012@84 8f0689cb-c7f0-493e-b951-6e7372201282

--- OOP5/src/OrderedMap.java
+++ OOP5/src/OrderedMap.java
@@ -1,2 +1,20 @@
+import java.util.Iterator;
+
 public class OrderedMap<K, V> extends OrderedSet<K> {
+	public void insert(K key, V value) {
+	}
+	
+	public Iterator<Entry<K, V>> iterator() {
+		return new EntryIterator();
+	}
+	
+	public static class Entry<K, V> {
+		
+	}
+	
+	private static class EntryIterator<K, V> {
+		public EntryIterator() {
+			
+		}
+	}
 }
--- OOP5/src/Set.java
+++ OOP5/src/Set.java
@@ -1,10 +1,62 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Set<T> implements Iterable<T> {
-	public void insert(T item) {
+	private Item<T> head = null;
+	
+	public void insert(T value) {
+		head = new Item<T>(value, head);
 	}
 
 	public Iterator<T> iterator() {
-		return null;
+		return new ItemIterator<T>(head);
+	}
+
+	private static class Item<T> {
+		private Item<T> next = null;
+		private T value = null;
+		
+		public Item(T value, Item<T> next) {
+			this.value = value;
+			this.next = next;
+		}
+		
+		public T getValue() {
+			return value;
+		}
+		
+		public Item<T> getNext() {
+			return next;
+		}
+	}
+	
+	private static class ItemIterator<T> implements Iterator<T> {
+		private Item<T> current;
+		
+		public ItemIterator(Item<T> current) {
+			this.current = current;
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return current.getNext() != null;
+		}
+
+		@Override
+		public T next() {
+			if (current == null)
+				throw new NoSuchElementException();
+			
+			T result = current.getValue();
+			current = current.getNext();
+			
+			return result;
+		}
+
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+		}
+		
 	}
 }
