8fd7ffb8e623e1213b5dbb8b67c7e7d620888f75
Dominik Dopplinger <Dominik Dopplinger@8f0689cb-c7f0-493e-b951-6e7372201282>
2012-11-27 18:43:25 +0000

    git-svn-id: https://subversion.assembla.com/svn/oop-ws2012@127 8f0689cb-c7f0-493e-b951-6e7372201282

--- OOP6/src/Verwaltung.java
+++ OOP6/src/Verwaltung.java
@@ -3,73 +3,71 @@ import java.util.LinkedHashMap;
 
 /**
  * Die Liste aus der Angabe (zur Speicherung der Androiden)
+ * 
  * @author Simon
- *
+ * 
  */
 public class Verwaltung {
-	
-	private LinkedHashMap<Integer,ANAndroide> map;
-	
+
+	private LinkedHashMap<Integer, ANAndroide> map;
 
 	public Verwaltung()
 	{
-		map=new LinkedHashMap<Integer,ANAndroide>();
+		map = new LinkedHashMap<Integer, ANAndroide>();
 		//null ist standardmaessig drinnen... muss bei jeder Ausgabe weggenommen werden.
 		map.put(null, null);
 	}
-	
-	
+
 	/**
 	 * Speichert Androiden in die Liste.
 	 * Falls ID schon vorhanden, wird statdessen ein Update vorgenommen
+	 * 
 	 * @param a
 	 */
 	public void insert(ANAndroide a)
 	{
-		
-		
-		ANAndroide old=map.get(a.getID());
-		if(old==null)//neuen Androiden einfuegen
+
+		ANAndroide old = map.get(a.getID());
+		if (old == null)//neuen Androiden einfuegen
 		{
 			System.out.println("Neuer Androide soll eingefuegt werden:");
-			System.out.println("Starte checks fuer Androiden mit ID "+a.getID());
+			System.out.println("Starte checks fuer Androiden mit ID " + a.getID());
 			a.checkSkin();
 			map.put(a.getID(), a);
-			a.addToHistory(a);
-			return;
+		} else {
+			//Androide soll update erhalten
+			System.out.println("Androide mit ID " + a.getID() + " soll Update erhalten:");
+			old.checkHauptTyp(a); //checke ob sich der Haupttyp aendert
+			a.checkSoftwareSecurityLevel(old.getSoftwareSecurityLevel()); // checke, ob dich das Sicherheitslevel der Software aendert
+			map.put(a.getID(), a);
+			map.get(a.getID()).copyHistory(old);
 		}
-		
-		//Androide soll update erhalten
-		System.out.println("Androide mit ID "+ a.getID()+" soll Update erhalten:");
-		old.checkHauptTyp(a); //checke ob sich der Haupttyp aendert
-		a.checkSkin();
-		map.put(a.getID(), a);
-		map.get(a.getID()).copyHistory(old);
-		
+
 	}
-	
-	
+
 	public String find(int ID)
 	{
-		ANAndroide a=map.get(ID);
-		if(a==null) return new String("Androide mit ID '"+ID+"' nicht vorhanden.");
+		ANAndroide a = map.get(ID);
+		if (a == null)
+			return new String("Androide mit ID '" + ID + "' nicht vorhanden.");
 		return a.toString();
 	}
-	
+
 	public void printHistory(int ID)
 	{
-		ANAndroide a=map.get(ID);
-		if(a!=null) a.printHistory(); 
+		ANAndroide a = map.get(ID);
+		if (a != null)
+			a.printHistory();
 	}
-	
+
 	public void test()
 	{
-		System.out.println(map.size()-1); //-1 wegen null-entry im Konstruktor
+		System.out.println(map.size() - 1); //-1 wegen null-entry im Konstruktor
 	}
-	
+
 	public Iterator<ANAndroide> iterator()
 	{
-		return map.values().iterator();		
+		return map.values().iterator();
 	}
-	
+
 }
